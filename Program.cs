c273aaf7b474bab8c5aeef107dd81c418ead4b2f
Marco Ustarroz <marcoustarroz@gmail.com>
2021-05-22 15:08:59 -0300

    First commit - Quark Eval

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamenQuark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
